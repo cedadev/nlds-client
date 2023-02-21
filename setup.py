b5603dead2ea99fdad9b032faf590c2b4571f5f3
Neil Massey <neil.massey@stfc.ac.uk>
2023-02-21 17:07:47 +0000

    added find_packages (finally fixed install problem)

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
     README = readme.read()
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 setup(
     name='nlds_client',
     version='0.0.2',
-    packages=['nlds_client'],
+    packages=find_packages(),
     install_requires=[
         'requests',
         'requests_oauthlib',
